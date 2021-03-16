7b6695108b3445cfcaf3d1d91ff360422a9b38c4
Ben Scott <ben.scott@shopify.com>
2021-03-16 15:34:50 -0700

    More Rollup WIP
    - Defines an inline babel config
    - Creates cjs and esm builds

--- config/sewing-kit/rollup-plugin.ts
+++ config/sewing-kit/rollup-plugin.ts
@@ -1,48 +1,70 @@
 import 'regenerator-runtime';
 import {createProjectBuildPlugin} from '@sewing-kit/plugins';
-import {ProjectKind} from '@sewing-kit/core';
+import {Package, ProjectKind} from '@sewing-kit/core';
 import {rollup, InputOptions, OutputOptions} from 'rollup';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 
-export function rollupPlugin(entrypoints: InputOptions['input']) {
-  return createProjectBuildPlugin('Package.Rollup', ({api, hooks, project}) => {
-    // Only working on packages
-    if (project.kind !== ProjectKind.Package) {
-      return;
-    }
+export function rollupPlugin() {
+  return createProjectBuildPlugin<Package>(
+    'Package.Rollup',
+    ({api, hooks, project}) => {
+      // Only working on packages
+      if (project.kind !== ProjectKind.Package) {
+        return;
+      }
 
-    hooks.steps.hook(steps => [
-      ...steps,
-      api.createStep(
-        {id: 'Rollup', label: 'Building the package with Rollup'},
-        async step => {
-          // await step.exec('echo', ['building the package with rollup'], {
-          //   stdio: 'inherit',
-          // })
+      hooks.steps.hook(steps => [
+        ...steps,
+        api.createStep(
+          {id: 'Rollup', label: 'Building the package with Rollup'},
+          async step => {
+            // TODO: get sk1 babel config from plugin-javascript?
+            const buildRoot = project.fs.buildPath();
 
-          // TODO: get sk1 babel config from plugin-javascript
-          // const babelConfig = await con;
-
-          await build(
-            {
+            const inputOptions: InputOptions = {
               input: project.entries.map(entry =>
                 require.resolve(entry.root, {paths: [project.root]}),
               ),
               plugins: [
                 nodeResolve({
                   extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                  // Only resolve files paths starting with a .
+                  // This treats every other path - i.e. modules like
+                  // `@shopify/address` or node built-ins like `path` as
+                  // externals that shoould not be bundled.
+                  resolveOnly: [/^\./],
                 }),
                 commonjs(),
+                babel({
+                  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                  envName: 'production',
+                  exclude: 'node_modules/**',
+                  babelHelpers: 'bundled',
+                  configFile: false,
+                  presets: [
+                    [
+                      '@shopify/babel-preset/web',
+                      {modules: 'auto', typescript: true},
+                    ],
+                    ['@shopify/babel-preset/react'],
+                  ],
+                }),
               ],
-              external: [],
-            },
-            [],
-          );
-        },
-      ),
-    ]);
-  });
+            };
+
+            const outputOptionsArray: OutputOptions[] = [
+              {format: 'cjs', dir: `${buildRoot}/cjs`, preserveModules: true},
+              {format: 'esm', dir: `${buildRoot}/esm`, preserveModules: true},
+            ];
+
+            await build(inputOptions, outputOptionsArray);
+          },
+        ),
+      ]);
+    },
+  );
 }
 
 async function build(
@@ -51,7 +73,7 @@ async function build(
 ) {
   console.log(
     'Building witth rollup!',
-    JSON.stringify({inputOptions, outputOptionsArray}),
+    // JSON.stringify({inputOptions, outputOptionsArray}, null, 2),
   );
   // create a bundle
   const bundle = await rollup(inputOptions);
